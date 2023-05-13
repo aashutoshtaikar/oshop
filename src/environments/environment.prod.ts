2dfab318d24a76aa9b2e090330faf517b37c59ea
aashutoshtaikar <aashutoshtaikar@gmail.com>
2023-05-13 10:57:59 -0400

    initial setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAhydRHOX0iv7Y1wtA3Cl2BScU9vIoF0_8",
+    authDomain: "oshop-49bc4.firebaseapp.com",
+    projectId: "oshop-49bc4",
+    storageBucket: "oshop-49bc4.appspot.com",
+    messagingSenderId: "491107995504",
+    appId: "1:491107995504:web:5a9f14ba502da65448c9b5",
+    measurementId: "G-5ZHTGZB583"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAhydRHOX0iv7Y1wtA3Cl2BScU9vIoF0_8",
+    authDomain: "oshop-49bc4.firebaseapp.com",
+    projectId: "oshop-49bc4",
+    storageBucket: "oshop-49bc4.appspot.com",
+    messagingSenderId: "491107995504",
+    appId: "1:491107995504:web:5a9f14ba502da65448c9b5",
+    measurementId: "G-5ZHTGZB583"
+  }
 };
 
 /*
